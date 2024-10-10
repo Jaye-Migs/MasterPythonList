170226d84b522090974cf2d5315ee1d87bbf829f
Mj Migueh <mjmigz31@gmail.com>
2024-10-10 14:42:25 +0800

    Add University Table to repository

---
+++ UniversityTable.py
@@ -0,0 +1,9 @@
+#Python List
+ID = [1, 2, 3, 4, 5]
+Name = ['University of the Philippines', 'Ateneo De Manila University', 'De La Salle University', 'University of Santo Tomas', 'Polytechnic University of the Philippines']
+Location = ['Quezon', 'Quezon', 'Manila', 'Manila', 'Manila']
+EstablishedYear = [1908, 1859, 1911, 1611, 1904]
+Type = ['Public', 'Private', 'Private', 'Private', 'Public']
+Website = ['www.up.edu.ph', 'www.ateneo.edu', 'www.dlsu.edu.ph', 'www.ust.edu.ph', 'www.pup.edu.ph']
+
+print(ID, Name, Location, EstablishedYear, Type, Website)
