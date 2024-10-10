b1c536204f812e3a4650331ee36e06754685e002
Mj Migueh <mjmigz31@gmail.com>
2024-10-10 14:53:00 +0800

    Add Restaurant Table to repository

---
+++ RestaurantTable.py
@@ -0,0 +1,9 @@
+#Python List
+ID = [1, 2, 3, 4, 5]
+Name = ['Vikings Luxury Buffet', 'Antonio\'s Restaurant', 'Mesa Filipino Moderne', 'Manam Comfort Filipino', 'Ramen Nagi']
+Location = ['Pasay City', 'Tagaytay', 'Makati City', 'Quezon City', 'Various Locations']
+CuisineType = ['Buffet', 'Fine Dining', 'Filipino', 'Filipino', 'Japanese']
+EstablishedYear = [2011, 2002, 2009, 2013, 2013]
+Website = ['www.vikings.ph', 'www.antoniosrestaurant.ph', 'www.mesa.ph', 'www.manam.ph', 'www.ramennagi.com.ph']
+
+print(ID, Name, Location, CuisineType, EstablishedYear, Website)
