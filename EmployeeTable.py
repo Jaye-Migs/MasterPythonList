0469e142c0d7d0e0a58646727471ebdfa9e1c167
Mj Migueh <mjmigz31@gmail.com>
2024-10-10 14:24:37 +0800

    Add Employee Table to repository

---
+++ EmployeeTable.py
@@ -0,0 +1,8 @@
+#Python List
+ID = [1, 2, 3, 4, 5]
+Name = ['John Doe', 'Jane Smith', 'Mark Johnson', 'Lisa Wong', 'Paul McDonald']
+Department = ['Sales', 'Human Resource', 'IT', 'Marketing', 'Finance']
+Age = [30, 25, 40, 28, 35]
+Email = ['john.doe@company.com', 'jane.smith@company.com', 'mark.johnson@company.com', 'lisa.wong@company.com', 'paul.mcdonald@company.com']
+
+print(ID, Name, Department, Age, Email)
