85b0eb2083246bb3ee3dcc34f184dcf4f76710c3
Mj Migueh <mjmigz31@gmail.com>
2024-10-10 14:12:51 +0800

    Add Product Table to repository

---
+++ ProductTable.py
@@ -0,0 +1,9 @@
+#Python Lists
+ID = [1, 2, 3, 4, 5]
+Name = ['Laptop', 'Desk Chair', 'Smartwatch', 'Notebook', 'Running Shoes']
+Category = ['Electronics', 'Furniture,' 'Electronics,' 'Stationery Apparel']
+Price = [750, 100, 200, 5, 80]
+Stock = [50, 200, 150, 500, 100]
+SupplierEmail = ['supplier1@gmail.com', 'supplier2@gmail.com', 'supplier3@gmail.com', 'supplier4@gmail.com', 'supplier5@gmail.com']
+
+print(ID, Name, Category, Price, Stock, SupplierEmail)
